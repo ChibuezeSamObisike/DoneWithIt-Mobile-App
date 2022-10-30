fbecf80324a551a6a9f862460432630eebc085b9
ChibuezeSamObisike <samobisike@gmail.com>
2022-10-30 19:03:10 +0100

    feat: adding the image picker component

--- App.js
+++ App.js
@@ -1,39 +1,40 @@
-// import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Platform,
-  StatusBar,
-  Text,
-  View,
-  TextInput,
-} from "react-native";
-import AppPicker from "./src/components/Picker";
-import AppTextInput from "./src/components/TextInput";
-
+import { useEffect, useState } from "react";
+import { Button, Text, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import Screen from "./src/components/Screen";
-import AccountScreen from "./src/screens/AccountScreen";
-import ListingEdit from "./src/screens/ListingEditScreen";
-import ListingScreen from "./src/screens/ListingScreen";
-import LoginScreen from "./src/screens/LoginScreen";
-import MessagesScreen from "./src/screens/MessagesScreen";
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-];
+import ImageInput from "./src/components/ImageInput";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
-  const [category, setCategory] = useState(categories[0]);
-  return <ListingScreen />;
-}
+  const [imageUri, setImageUri] = useState();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : "0%",
-  },
-});
+  const requestPermission = async () => {
+    const result = await ImagePicker.requestCameraPermissionsAsync();
+    if (!result.granted) {
+      alert("You need to enable permission");
+    }
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (error) {
+      console.log("Error reading an image");
+    }
+  };
+  return (
+    <Screen>
+      <Button title='Select Image' onPress={selectImage} />
+      <Image
+        source={{ uri: imageUri }}
+        style={{ width: "130%", height: 400 }}
+      />
+      <ImageInput imageUri={imageUri} />
+    </Screen>
+  );
+}
---
+++ src/components/ImageInput.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
+import colors from "../config/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const ImageInput = ({ imageUri }) => {
+  const handlePress = () => {};
+  return (
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            name='camera'
+            size={40}
+            color={colors.medium}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 100,
+    width: 100,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ImageInput;
